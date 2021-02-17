8d8039a7aed8b4fe096f97943ec82a455a653811
PabloRomero99 <byscotexx@gmail.com>
2021-02-17 13:55:52 +0100

    Creada estructura del proyecto.

---
+++ src/test/java/principal/AritmeticaTest.java
@@ -0,0 +1,26 @@
+package principal;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AritmeticaTest {
+
+    @org.junit.jupiter.api.Test
+    void suma() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void resta() {
+    }
+
+    @Test
+    @DisplayName("La multiplitisaoh ")
+    void multiplicacion() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void division() {
+    }
+}
