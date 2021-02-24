6910b5fb93daf84fc47fb5654501f7285ba5cf68
sose1102000 <sose1102@gmail.com>
2021-02-24 18:21:46 +0100

    cuadraticatest y normal

--- src/main/java/principal/EcuacionCuadratica.java
+++ src/main/java/principal/EcuacionCuadratica.java
@@ -1,5 +1,39 @@
 package principal;
 
 public class EcuacionCuadratica {
+    private int a, b;
 
+    public EcuacionCuadratica(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+
+    public boolean haySolucion(){
+        if (this.a==0)
+            return this.b == 0;
+        return true;
+    }
+
+    public double solucion(){
+        if (this.a == 0 && this.b == 0) {
+            return Double.POSITIVE_INFINITY;
+        }
+        return (this.b*(-1.0)/this.a);
+    }
 }
--- src/test/java/principal/EcuacionCuadraticaTest.java
+++ src/test/java/principal/EcuacionCuadraticaTest.java
@@ -1,7 +1,33 @@
 package principal;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class EcuacionCuadraticaTest {
 
+
+    @Test
+    void getA() {
+    }
+
+    @Test
+    void getB() {
+    }
+
+    @Test
+    void setA() {
+    }
+
+    @Test
+    void setB() {
+    }
+
+    @Test
+    void haySolucion() {
+    }
+
+    @Test
+    void solucion() {
+    }
 }
